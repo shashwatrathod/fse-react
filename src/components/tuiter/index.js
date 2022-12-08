2fa964b59740602a068fd3d292c3cf67368e34ae
Shashwat <shashwatrathod22@gmail.com>
2022-12-08 08:03:39 -0500

    add dislikes page

--- src/components/tuiter/index.js
+++ src/components/tuiter/index.js
@@ -17,6 +17,7 @@ import Movies from "../movies";
 import MovieDetails from "../movies/details";
 import MyTuits from "../profile/my-tuits";
 import MyLikes from "../profile/my-likes";
+import MyDislikes from "../profile/my-dislikes";
 
 function Tuiter() {
   return (
@@ -42,6 +43,7 @@ function Tuiter() {
               <Route path="/profile" element={<Profile />} />
               <Route path="/profile/tuits" element={<MyTuits />} />
               <Route path="/profile/likes" element={<MyLikes />} />
+              <Route path="/profile/dislikes" element={<MyDislikes />} />
               <Route path="/profile/edit" element={<EditProfile />} />
               <Route path="/movies" element={<Movies />} />
               <Route path="/movies/:imdbID" element={<MovieDetails />} />
